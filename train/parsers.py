829776fefca628b4f045e446ca0ffc4445512833
Kate <kshajash@gmail.com>
2022-08-13 09:16:45 -0700

    Add parsers file and make modular to work w/ sweep

---
+++ train/parsers.py
@@ -0,0 +1,143 @@
+import argparse
+
+
+def pop_arguments(config, keys):
+    """Split arguments from config dict
+
+    Args:
+        config (dict_): Dictionary to pop keys from
+        keys (list of strings): List of keys to pop from config
+
+    Returns:
+        (dict, dict): dictionary minus keys, dictionary of keys
+    """
+    alg_config = {}
+    for hparam in keys:
+        if hparam in config:
+            val = config.pop(hparam)
+            alg_config[hparam] = val
+    return config, alg_config
+
+
+def setup_base_parser():
+    
+    parser = argparse.ArgumentParser(add_help=False)
+
+    parser.add_argument(
+        "--config",
+        default="./configs/ppo-mrp-v0.json", 
+        type=str,
+        help="Config json filename.",
+    )
+    parser.add_argument(
+        "--seed",
+        default=17,
+        type=int,
+        help="Random seed. (int, default = 17)",
+    )
+    parser.add_argument(
+        "--total_timesteps",
+        default=1_000_000,
+        type=int,
+        help="Number of timesteps to run for training stable baselines. (int, default = 1M)",
+    )
+    parser.add_argument(
+        "--disable_wandb",
+        action="store_true",
+        help="Do not initialize wandb logger"
+    )
+    # parser.add_argument(
+    #     "--new_step_api",
+    #     action="store_true",
+    #     help="Use new step API in OpenAI env (new_obs, rew, term, trunc, info) vs (new_obs, rew, done, info)"
+    # )
+    parser.add_argument(
+        "--render",
+        action="store_true",
+        help="Render env in testing"
+    )
+    parser.add_argument(
+        "--save_model",
+        action="store_true",
+        help="Save model when done.",
+    )
+    parser.add_argument(
+        "--run_wandb_sweep",
+        action="store_true",
+        help="Run wandb sweep and use alg parser to overwrite config.",
+    )
+    parser.add_argument(
+        "--n_envs",
+        default=1,
+        type=int,
+    )
+
+    return parser
+
+
+def setup_PPO_parser(parent_parser):
+    """Setup parser including PPO hparams. Use this when using sweep from wandb, otherwise
+    this will override the config file settings.
+
+    Args:
+        parent_parser (ArgumentParser): Parent parser to add onto.
+
+    Returns:
+        ArgumentParser: parser with PPO arguments added
+    """
+    
+    parser = argparse.ArgumentParser(parents=[parent_parser])
+    parser.add_argument(
+        "--learning_rate",
+        default=0.0003, 
+        type=float,
+    )
+    parser.add_argument(
+        "--clip_range",
+        default=0.2,
+        type=float,
+    )
+    parser.add_argument(
+        "--batch_size",
+        default=128,
+        type=int,
+    )
+    parser.add_argument(
+        "--n_epochs",
+        default=10,
+        type=int,
+    )
+    parser.add_argument(
+        "--ent_coef",
+        default=0.01,
+        type=float
+    )
+    parser.add_argument(
+        "--n_steps",
+        default=4096,
+        type=int,
+    )
+    parser.add_argument(
+        "--max_grad_norm",
+        default=0.5,
+        type=float,
+    )
+    parser.add_argument(
+        "--vf_coef",
+        default=0.5,
+        type=float
+    )
+
+    return parser
+
+
+PPO_HPARAMS = [
+        'learning_rate',
+        'clip_range',
+        'batch_size',
+        'n_epochs',
+        'ent_coef',
+        'n_steps',
+        'max_grad_norm',
+        'vf_coef'
+    ]
--- train/train.py
+++ train/train.py
@@ -8,150 +8,42 @@ import wandb
 from wandb.integration.sb3 import WandbCallback
 import torch
 
+import parsers as parse
 import gym
 import gym_puzzles
-import argparse
-
-def setup_training_parser():
-    
-    parser = argparse.ArgumentParser(add_help=False)
-
-    parser.add_argument(
-        "--config",
-        default="./configs/ppo-mrp-v0.json", 
-        type=str,
-        help="Save model when done.",
-    )
-    parser.add_argument(
-        "--seed",
-        default=17,
-        type=int,
-        help="Random seed. (int, default = 17)",
-    )
-    parser.add_argument(
-        "--total_timesteps",
-        default=10000000,
-        type=int,
-        help="Number of epochs to run the training. (int, default = 75)",
-    )
-    parser.add_argument(
-        "--disable_wandb",
-        action="store_true",
-        help="Do not initialize wandb logger"
-    )
-    parser.add_argument(
-        "--new_step_api",
-        action="store_true",
-        help="Use new step API in OpenAI env (new_obs, rew, term, trunc, info) vs (new_obs, rew, done, info)"
-    )
-    parser.add_argument(
-        "--render",
-        action="store_true",
-        help="Render env in testing"
-    )
-    parser.add_argument(
-        "--save_model",
-        action="store_true",
-        help="Save model when done.",
-    )
-    parser.add_argument(
-        "--n_envs",
-        default=4,
-        type=int,
-    )
-
-    return parser
-
-
-def setup_alg_params_parser(parent_parser):
-    
-    parser = argparse.ArgumentParser(parents=[parent_parser])
-    parser.add_argument(
-        "--learning_rate",
-        default=0.0003, 
-        type=float,
-    )
-    parser.add_argument(
-        "--clip_range",
-        default=0.2,
-        type=float,
-    )
-    parser.add_argument(
-        "--batch_size",
-        default=128,
-        type=int,
-    )
-    parser.add_argument(
-        "--n_epochs",
-        default=10,
-        type=int,
-    )
-    parser.add_argument(
-        "--ent_coef",
-        default=0.01,
-        type=float
-    )
-    parser.add_argument(
-        "--n_steps",
-        default=4096,
-        type=int,
-    )
-    parser.add_argument(
-        "--max_grad_norm",
-        default=0.5,
-        type=float,
-    )
-    parser.add_argument(
-        "--vf_coef",
-        default=0.5,
-        type=float
-    )
-
-    return parser
-    # return parser.parse_args()
 
 
 def main():
 
-    ALG_HPARAM_NAMES = [
-        'learning_rate',
-        'clip_range',
-        'batch_size',
-        'n_epochs',
-        'ent_coef',
-        'n_steps',
-        'max_grad_norm',
-        'vf_coef'
-    ]
+    # Set this to True if you want to override config params with commandline params
+    RUN_WANDB_SWEEP = False
 
-    parent_parser = setup_training_parser()
-    parser = setup_alg_params_parser(parent_parser)
-    args = parser.parse_args()
-    print("args", args)
-    cl_config = vars(args)
+    # Setup argument parsers - go to parsers.py for more info or use --help
+    parent_parser = parse.setup_base_parser()
+    parser = parse.setup_PPO_parser(parent_parser)
+    cl_config = vars(parser.parse_args())
 
-    # Split out algorithm config
-    alg_config = {}
-    for hparam in ALG_HPARAM_NAMES:
-        if hparam in cl_config:
-            val = cl_config.pop(hparam)
-            alg_config[hparam] = val
+    # Split alg params from base params
+    cl_config, alg_config = parse.pop_arguments(cl_config, parse.PPO_HPARAMS)
     
-    print("\nalg config: ", alg_config)
-
-    print("\ncl config: ", cl_config)
+    # print("\nalg config: ", alg_config)
+    # print("\ncl config: ", cl_config)
     
     # Load config file
-    with open(args.config, "r") as f:
+    with open(cl_config['config'], "r") as f:
         config = json.load(f)
 
+    # Update base config with commandline params 
     config.update(**cl_config)
-    config['alg_params'].update(**alg_config)
+
+    # Overwrite config with commandline params 
+    if RUN_WANDB_SWEEP:
+        config['alg_params'].update(**alg_config)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Running model on device {device}")
 
-    use_wandb = not args.disable_wandb
+    use_wandb = not config['disable_wandb']
 
     if use_wandb:
         run = wandb.init(
@@ -202,9 +94,11 @@ def main():
     model.learn(
         total_timesteps=config["total_timesteps"],
         log_interval=4,
+        # NOTE: none of these callbacks are working - FIX THIS
         callback=WandbCallback(
             gradient_save_freq=10,
             log='all',
+            model_save_freq=10,
             model_save_path=f"models/{run.id}",
             verbose=2,
         ),
@@ -242,21 +136,6 @@ def main():
     # )
     model.save("ppo_puzzles_v2")
 
-    del model # remove to demonstrate saving and loading
-
-
-    model = PPO.load("ppo_puzzles_v2")
-
-    obs = env.reset()
-    for i in range(5):
-        obs = env.reset()
-        while True:
-            action, _states = model.predict(obs, deterministic=True)
-            obs, reward, done, info = env.step(action)
-            env.render()
-            if done:
-                break
-    env.close()
 
 
 if __name__ == "__main__":
